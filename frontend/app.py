13da9bb40af02763add0f69b37ccbb41fcc4501c
Rani shreyashi <rani_s@ph.iitr.ac.in>
2026-06-23 20:37:45 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import random
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://insightiq-lah8.onrender.com/"
 
 st.set_page_config(
     page_title="InsightIQ",
